a4e7a8bef8ad4157782c2172c294c39eb88176f1
oso821 <oso821@naver.com>
2025-12-14 21:06:03 +0900

    A.java / C.java commit in Alice branch

--- GitHw_1/src/mygit/A.java
+++ GitHw_1/src/mygit/A.java
@@ -3,5 +3,6 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("과제 A.Java 초기버전입니다.");
+		System.out.println("과제 A.Java Alice branch 추가버전입니다.");
 		}
 }
--- GitHw_1/src/mygit/C.java
+++ GitHw_1/src/mygit/C.java
@@ -2,6 +2,6 @@ package mygit;
 
 public class C {
 	public static void main(String[] args) {
-		System.out.println("");
+		System.out.println("과제 C.java Alice branch 추가버전입니다.");
 		}
 }
