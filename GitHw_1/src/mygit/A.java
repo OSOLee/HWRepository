895be85e00c2e14bc450a4dc7564d36dd8b68250
oso821 <oso821@naver.com>
2025-12-14 21:09:44 +0900

    (6) commit A.java / B.java in Bob branch

--- GitHw_1/src/mygit/A.java
+++ GitHw_1/src/mygit/A.java
@@ -3,5 +3,6 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("과제 A.Java 초기버전입니다.");
+		System.out.println("(6) Bob branch A.Java 수정버전입니다.");
 		}
 }
--- GitHw_1/src/mygit/B.java
+++ GitHw_1/src/mygit/B.java
@@ -1,5 +1,7 @@
 package mygit;
 
 public class B {
-
+	public static void main(String[] args) {
+		System.out.println("(6) Bob branch B.Java 수정버전입니다.");
+	}
 }
